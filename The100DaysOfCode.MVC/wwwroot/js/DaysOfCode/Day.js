a56ff0497980fd85321bfb11b82d01ad736fa2fc
Spoomer <renerahnfeld@gmail.com>
2022-01-28 17:51:48 +0100

    line trough on marked goals

--- The100DaysOfCode.MVC/wwwroot/js/DaysOfCode/Day.js
+++ The100DaysOfCode.MVC/wwwroot/js/DaysOfCode/Day.js
@@ -1,8 +1,17 @@
 document.querySelectorAll("input[goalId]")
 .forEach(function (check) {
     check.addEventListener('change', function (event) {
-        const dataObj = { id: check.getAttribute("goalid"), isChecked: check.checked }
+        const goalId = check.getAttribute("goalid");
+        const dataObj = { id: goalId, isChecked: check.checked }
         fetchPutFormData('/DaysOfCode/CheckGoal', dataObj)
+        if (check.checked)
+        {
+            document.getElementById("goal_"+goalId).classList.add("text-decoration-line-through");
+        }
+        else
+        {
+            document.getElementById("goal_"+goalId).classList.remove("text-decoration-line-through")
+        }
     });
 });
 
